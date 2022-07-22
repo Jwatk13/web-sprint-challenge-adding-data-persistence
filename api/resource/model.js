fc70a0fc8eba97a3931bd6fadfdfd8c521caed22
Joseph Watkins <josephxwatkins@gmail.com>
2022-07-22 15:17:18 -0400

    resource router

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,4 +1,16 @@
 // build your `Resource` model here
 const db = require('../../data/dbConfig');
 
-module.exports = {}
+function getResources() {
+    return db('resources')
+}
+
+function postResource(resource) {
+    return db('resources')
+        .insert(resource)
+        .then(([resource_id]) => {
+            return db('resources').where('resource_id', resource_id).first();
+        })
+}
+
+module.exports = { getResources, postResource }
--- api/resource/router.js
+++ api/resource/router.js
@@ -2,4 +2,12 @@
 const router = require('express').Router();
 const Resource = require('./model');
 
+router.get('/', (req, res, next) => {
+    Resource.getResources()
+        .then(resources => {
+            res.json(resources)
+        })
+        .catch(next)
+})
+
 module.exports = router;
